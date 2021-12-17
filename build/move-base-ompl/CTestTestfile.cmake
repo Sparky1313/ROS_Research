9bce264cc094eaf210cc2209c62ddce7d5f5ee8f
Trevor Foland <50787265+Sparky1313@users.noreply.github.com>
2021-12-17 09:32:34 -0500

    Delete build directory

--- build/move-base-ompl/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/walle/catkin_ws/src/move-base-ompl
-# Build directory: /home/walle/catkin_ws/build/move-base-ompl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/spawn_obstacles/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/walle/catkin_ws/src/spawn_obstacles
-# Build directory: /home/walle/catkin_ws/build/spawn_obstacles
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
